61381dacd24c13a5b5c1ea7d833b3c194c491a8f
Karishma Gupta <karishmaneha42@gmail.com>
2025-03-05 16:57:34 +0530

    Add NewsAPI Interface

---
+++ app/src/main/java/com/example/newsapp/api/NewsAPI.kt
@@ -0,0 +1,31 @@
+package com.example.newsapp.api
+
+import com.example.newsapp.models.NewsResponse
+import com.example.newsapp.util.Contants.Companion.API_KEY
+import okhttp3.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+    @GET("v2/top-headlines")
+    suspend fun getHeadlines(
+        @Query("Country")
+        CountryCode: String = "US",
+        @Query("Page")
+        PageNunber: Int = 1,
+        @Query("apiKEY")
+        apiKey: String = API_KEY,
+    ):NewsResponse
+
+    @GET("")
+    suspend fun searchForNews(
+        @Query("q")
+       searchQuery: String,
+        @Query("Page")
+        PageNunber: Int = 1,
+        @Query("apiKEY")
+        apiKey: String = API_KEY,
+    ): NewsResponse
+
+
+}
